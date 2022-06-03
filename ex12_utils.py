40d49902076421214ce4cee069f20bbb2ab463ff
tomer <tomer.avrahami@me.com>
2022-06-03 10:24:35 +0300

    stemi

--- ex12_utils.py
+++ ex12_utils.py
@@ -2,6 +2,8 @@ def is_valid_path(board, path, words):
     pass
 
 def find_length_n_paths(n, board, words):
+    for cell in board:
+        _helper()
     pass
 
 def find_length_n_words(n, board, words):
@@ -9,3 +11,10 @@ def find_length_n_words(n, board, words):
 
 def max_score_paths(board, words):
     pass
+
+
+def get_neighbors(location, board):
+
+
+def get_words(path)
+    - create dict by length
