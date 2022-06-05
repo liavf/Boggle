a3db4ad1b2a447731abe4b56d75d62901eaacedf
tomer <tomer.avrahami@me.com>
2022-06-05 20:08:29 +0300

    stemi

--- boggle.py
+++ boggle.py
@@ -1,20 +1,113 @@
 from ex12_utils import *
 from boggle_board_randomizer import *
-WORDS_PATH = "boogle_dict.txt"
+import tkinter as tk
+from time import strftime
+WORDS_PATH = "boggle_dict.txt"
+IN_GUESS = 'red'
+TURN_TIME = 60*3 #SECONDS
 
 class Game:
     def __init__(self):
         self.board = randomize_board()
         self.words = get_relevant_words(WORDS_PATH, self.board)
-        self.score = score
-        self.guessed_paths = {}
+        self.score = 0
+        self.guesses = set() #words
+
 
     def run_round(self, guess):
-        #assuming guess is a valid path
-        word = get_word_from_path(self.board, guess)
-        if word in self.words:
-            guess
-    def main_loop(self, guess):
+        word = is_valid_path(self.board, guess, self.words)
+        if word:
+            guess.add(words)
+
+    def main_loop(self):
+        # while play:
+        pass
+
 
 class GameDisplay:
-    pass
+    def __init__(self, game_logic):
+        self.game_logic = game_logic
+        self.current_guess = ""
+        self._root = tk.Tk()
+        #title
+        self._root.title('@ grade us 105 @')
+        self._title = tk.Label(self._root, text = "Boggle", font = ("Curier",
+                                                                  30))
+        self._title.pack()
+        #play_game
+        self._play_button = tk.Button(self._root, text="Play", command = \
+                                self._play_round, font = ("Courier", 30))
+        self._play_button.pack(side=tk.BOTTOM)
+        #timer
+        self._timer = TURN_TIME
+        self._time_label = tk.Label(self._root, font = ("Courier", 20),
+                                    text=f"time: {self._timer // 60} mins"
+                                    f" {self._timer % 60} secs")
+        self._time_label.pack(side=tk.TOP)
+        #score
+        self._score = 0
+        self._score_label = tk.Label(self._root, font = ("Courier", 20),
+                                     text = f"score: {self._score}")
+        self._score_label.pack()
+        #currect selection label
+        self._display_label = tk.Label(self._root, font = ("Courier", 30))
+        self._display_label.pack(side=tk.TOP)
+        frame = tk.Frame(self._root)
+        frame.pack()
+        self.buttons = [tk.Button(frame) for _ in range(len(
+            self.game_logic.board)** 2)]
+        self._fill_board(frame)
+
+    def _fill_board(self, frame):
+        board = self.game_logic.board
+        indexes = get_all_indexes(len(board))
+        button_num = 0
+        for index in indexes:
+            x, y = index
+            letter = board[x][y]
+            #todo: change color
+            self.buttons[button_num].configure(text = letter, command =
+            self._button_event(letter, button_num), font = ("Courier", 30),
+                                               bg = "yellow")
+            #button[= tk.Button(frame, text = letter, command =
+            #self._button_event(letter, button_num), font = ("Courier", 30))
+            #print(button)
+            self.buttons[button_num].grid(row = x, column = y)
+            button_num += 1
+
+    def _button_event(self, letter, button_num):
+        def button_event_helper():
+            self.current_guess += letter
+            self._display_label.configure(text = self.current_guess)
+            self.buttons[button_num].configure(text = "-", bg = IN_GUESS)
+        return button_event_helper
+
+    def _play_round(self):
+        self._timer = TURN_TIME
+        self.countdown()
+        return
+
+    def start(self):
+        self._root.mainloop()
+
+    def countdown(self):
+        if self._timer == 0:
+            self._time_label.configure(text="times up")
+        else:
+            self._time_label.configure(text=f"time: {self._timer//60} mins"
+                                            f" {self._timer%60} secs")
+            self._timer -= 1
+            self._root.after(1000, self.countdown) #every_second
+
+    # def _check_end(self):
+    # def _mouse_press(self):
+    # def _get_location_clicked(self):??
+    #
+    # def change_color(self, x, y, color):
+    # def show_score(self):
+    # def end_round(self):
+
+if __name__ == '__main__':
+    game_logic = Game()
+    gd = GameDisplay(game_logic)
+    gd.start()
--- ex12_utils.py
+++ ex12_utils.py
@@ -1,8 +1,12 @@
 import itertools
 from collections import Counter
+import time
 
 def get_word_from_path(board, path):
     return "".join([board[cell[0]][cell[1]] for cell in path])
+
+def get_from_location(board, location):
+    return board[location[0]][location[1]]
 
 def get_all_indexes(board_length):
     for x in range(board_length):
@@ -18,61 +22,47 @@ def get_neighbors(location, board_length):
                 neighbors.append((x+dx, y+dy))
     return neighbors
 
-
-
-def is_valid_path(board, path, words):
-    valid_path = True
-    if len(path) > 1:
-        index, next_index = 0, 1
-        while next_index < len(path) and valid_path:
-            if path[next_index] in get_neighbors(path[index], len(board)):
-                index += 1
-                next_index += 1
-            else:
-                valid_path = False
-    if valid_path:
-        word = get_word_from_path(board, path)
-        if word in words:
-            return word
-    return None
-
-
 def find_length_n_paths(n, board, words):
     paths = []
     for location in get_all_indexes(len(board)):
-        #print("getting path from", location)
-        paths_for_location = _find_path_helper(location, board, n, [location], [], words,
-                                       "path")
+        start_letter = get_from_location(board, location)
+        paths_for_location = _find_path_helper(location, board, n,
+                                               [location], start_letter,
+                                               [], words, "path")
         paths.extend(path for path in paths_for_location)
     return paths
 
-def _find_path_helper(start, board, n, path, all, words, find_by):
-    word = get_word_from_path(board, path)
+def _find_path_helper(start, board, n, curr_path, curr_word, paths,
+                      words, find_by):
     if find_by == "path":
-        key = path
+        key = curr_path
     else:
-        key = word
+        key = curr_word
 
     if len(key) > n:
         return
     elif len(key) == n:
-        if word in words: # count only real words
-            all.append(path[:])
+        if curr_word in words: # count only words from list
+            paths.append(curr_path[:])
     else:
         for location in get_neighbors(start, len(board)):
-            if location not in path:
-                path.append(location)
-                _find_path_helper(location, board, n, path, all, words, find_by)
-                path.remove(location)
-        return all
+            if location not in curr_path:
+                curr_path.append(location)
+                curr_word += get_from_location(board, location)
+                _find_path_helper(location, board, n, curr_path, curr_word,
+                                  paths, words, find_by)
+                curr_path.remove(location)
+                curr_word = curr_word[:len(curr_word) - 1]
+        return paths
 
 def find_length_n_words(n, board, words):
     paths = []
     for location in get_all_indexes(len(board)):
-        paths_for_location = _find_path_helper(location, board, n, [location], [], words,
-                                       "word")
-        if paths_for_location:
-            paths.extend(paths_for_location)
+        start_letter = get_from_location(board, location)
+        paths_for_location = _find_path_helper(location, board, n,
+                                               [location], start_letter,
+                                               [], words, "word")
+        paths.extend(path for path in paths_for_location)
     return paths
 
 def max_score_paths(board, words):
@@ -81,16 +71,88 @@ def max_score_paths(board, words):
     max_len = max([len(word) for word in words])
     min_len = min([len(word) for word in words])
     for n in range(max_len, min_len - 1, -1):
-        print(n)
         paths = find_length_n_paths(n, board, words)
-        print("got paths")
         for path in paths:
+            #todo: return words from recursion to avoid this step
             word = get_word_from_path(board, path)
             if word not in words_for_score:
-                print(word, path)
                 paths_for_score.append(path)
                 words_for_score.add(word)
     return calculate_score(paths_for_score)
+
+def find_up_to_n_paths(n, board, words):
+    paths = []
+    for location in get_all_indexes(len(board)):
+        start_letter = get_from_location(board, location)
+        paths_for_location = _find_up_to_n_paths_helper(location, board, n,
+                                                        [location], start_letter,
+                                                        [], words)
+        paths.extend(path for path in paths_for_location)
+    return paths
+
+def _find_up_to_n_paths_helper(start, board, n, curr_path, curr_word, paths,
+                        words):
+    if len(curr_word) > n:
+        return
+
+    if curr_word in words: # count only words from list
+        paths.append(curr_path[:])
+
+    for location in get_neighbors(start, len(board)):
+        if location not in curr_path:
+            curr_path.append(location)
+            curr_word += get_from_location(board, location)
+            _find_up_to_n_paths_helper(location, board, n, curr_path, curr_word,
+                                    paths, words)
+            curr_path.remove(location)
+            curr_word = curr_word[:len(curr_word) - 1]
+    return paths
+def max_score_paths_2(board, words):
+    paths_for_score = []
+    words_for_score = set()
+    n = max([len(word) for word in words])
+    paths = find_up_to_n_paths(n, board, words)
+    paths.sort(key=len, reverse=True)
+    for path in paths:
+         #todo: return from recursion
+        word = get_word_from_path(board, path)
+        if word not in words_for_score:
+            paths_for_score.append(path)
+            words_for_score.add(word)
+    return calculate_score(paths_for_score)
+
+# def find_paths_by_word(board, word):
+#     paths = []
+#     for location in get_all_indexes(len(board)):
+#         start_letter = get_from_location(board, location)
+#         if start_letter == word[0:]: #start only from relevant locations
+#             paths_for_location = _find_path_helper(location, board, n,
+#                                                 [location], start_letter,
+#                                                 [], [word], "path")
+#             paths.extend(path for path in paths_for_location)
+#     return paths
+
+# def max_score_paths_faster(board, words):
+#     paths_for_score = []
+#     words_for_score = set()
+#     max_len = max([len(word) for word in words])
+#     min_len = min([len(word) for word in words])
+#     for n in range(max_len, min_len - 1, -1):
+#         #search normaly from every index
+#         if n < THRESHOLD:
+#             paths = find_length_n_paths(n, board, words)
+#             for path in paths:
+#                 #todo: return from recursion
+#                 word = get_word_from_path(board, path)
+#                 if word not in words_for_score:
+#                     paths_for_score.append(path)
+#                     words_for_score.add(word)
+#         else:
+#             for word in [word for word in words if len(word) == n]:
+#                 paths = find_paths_by_word(board, word).sort(key=len)
+#                 if paths:
+#                     paths_for_score.append(paths[0]) #only longest path will be added
+#     return calculate_score(paths_for_score)
 
 def calculate_score(paths_for_score):
     score = 0
@@ -121,20 +183,29 @@ def get_relevant_words(path, board):
                 words.add(line.strip())
     return words
 
+
 if __name__ == '__main__':
     #test
     from boggle_board_randomizer import *
-    board = randomize_board()
-
+    #board = randomize_board()
+    board = [['Y', 'A', 'X', 'I'],
+     ['M', 'S', 'G', 'T'],
+     ['R', 'T', 'P', 'G'],
+     ['B', 'S', 'T', 'W']]
     from pprint import pprint
     pprint(board)
     #print(get_neighbors((2,2), len(board)))
 
-
     words = get_relevant_words("boggle_dict.txt", board)
+    start = time.time()
     print(max_score_paths(board, words))
+    first = time.time()
+    print(first-start, "seconds")
 
-    words = get_words("boggle_dict.txt")
-    print(max_score_paths(board, words))
+    print(max_score_paths_2(board, words))
+    second = time.time()
+    print(second-first, "seconds")
+    # words = get_words("boggle_dict.txt")
+    # print(max_score_paths(board, words))
 
 
